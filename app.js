60384020491b1bd8963c3831447edc7c6774b2a4
Anurag Tomar <anuragkt20@gmail.com>
2022-11-03 11:58:35 +0530

    updated cors settings

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-require('dotenv').config()
-require('express-async-errors')
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+require("express-async-errors");
+const cookieParser = require("cookie-parser");
 
-const path = require('path')
-const cors = require('cors');
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const session = require('express-session')
-const mongoDBSession = require('connect-mongodb-session')(session)
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const mongoDBSession = require("connect-mongodb-session")(session);
 
 // extra security packages
-const helmet = require('helmet')
-const xss = require('xss-clean')
-
+const helmet = require("helmet");
+const xss = require("xss-clean");
 
 app.get("/ip", (req, res) => res.send(req.ip));
 
@@ -24,147 +23,113 @@ app.use(
   helmet.contentSecurityPolicy({
     useDefaults: true,
     directives: {
+      "default-src": ["'self'", "https://anutom20-ecommerce.herokuapp.com"],
       "img-src": ["'self'", "https: data:"],
-    
     },
   })
 );
 app.use(xss());
 
-
-
-
-
-const port = process.env.PORT || 5000
-
-
+const port = process.env.PORT || 5000;
 
 // route controllers
-const authRouter = require('./routes/auth')
-const productRouter = require('./routes/products')
-const userRouter = require('./routes/users')
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/products");
+const userRouter = require("./routes/users");
 
 // middleware
-const errorHandlerMiddleware = require('./middleware/error-handler')
-const NotFoundMiddleware = require('./middleware/NotFoundMiddleware')
-
-
-
-
-
-
-
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const NotFoundMiddleware = require("./middleware/NotFoundMiddleware");
 
 // body parser
 
-app.use(express.json())
+app.use(express.json());
 
 // cookie parser
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-// enable all CORS requests 
+// enable all CORS requests
 
-app.use(cors({ credentials: true}));
-
-
+app.use(
+  cors({
+    credentials: true,
+    origin: ["https://anutom20-ecommerce.herokuapp.com", "http://localhost:5000"],
+  })
+);
 
 const store = new mongoDBSession({
-  uri : process.env.MONGO_URI,
-  collection: "mySessions"
-})
-
+  uri: process.env.MONGO_URI,
+  collection: "mySessions",
+});
 
 // app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: false,
-  store:store,
-  cookie: {maxAge: parseInt(process.env.MAX_AGE)},
-  rolling:true
-  
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: store,
+    cookie: { maxAge: parseInt(process.env.MAX_AGE) },
+    rolling: true,
+  })
+);
 
-const setClientCookie = (req,res,next)=>{
-  if(req.session.userId){
-    res.cookie("username", req.session.name , {
+const setClientCookie = (req, res, next) => {
+  if (req.session.userId) {
+    res.cookie("username", req.session.name, {
       maxAge: parseInt(process.env.MAX_AGE),
       httpOnly: false,
       secure: false,
       overwrite: true,
     });
   }
-  next()
-}
+  next();
+};
 
 app.use(setClientCookie);
 
-
-app.use(express.static(path.resolve(__dirname + '/client/build')));
- 
-
-
+app.use(express.static(path.resolve(__dirname + "/client/build")));
 
 // app.get('/', (req, res) => {
 //   res.json({message : "Hello world"})
 // })
 
-
 // routes
 
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/products', productRouter)
-app.use('/api/v1/users', userRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/users", userRouter);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-
-
-
-
-
-
 // app.get('/api/v1/test' , async (req, res) => {
 //   throw Error("access denied")
 // });
- 
+
 // app.use((err, req, res, next) => {
-  
+
 //   res.status(403).json({error : err.message})
-  
- 
+
 //   next(err);
 // });
-app.use(errorHandlerMiddleware)
-app.use(NotFoundMiddleware)
+app.use(errorHandlerMiddleware);
+app.use(NotFoundMiddleware);
 
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: true,
+    });
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-
-
-const start = async()=>{
- try{
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    autoIndex: true
-  })
-  app.listen(port, () => {
-  
-    console.log(`Server listening on port ${port}`)
-  })
- }
- catch(error){
-  console.log(error)
- } 
-
-}
-
-
-start()
-
-
-
-
+start();
